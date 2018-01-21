3464a0470a13a2133d642eb35d038c7945edcf10
corey@ginnivan.net <corey@ginnivan.net>
2018-01-21 18:31:53 +0800

    Remove shit

--- site.config.js
+++
@@ -1,9 +0,0 @@
-const projects = require('./src/data/recipes')
-
-module.exports = {
-  site: {
-    title: 'NanoGen',
-    description: 'Micro Static Site Generator in Node.js',
-    projects
-  }
-}
