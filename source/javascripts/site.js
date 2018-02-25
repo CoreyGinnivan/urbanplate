3a5c9b6f45395d2274f7d84d41863520ff9cdf87
corey@ginnivan.net <corey@ginnivan.net>
2018-02-25 16:53:25 +0800

    Install Clipboard JS

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,6 +1,13 @@
 // This is where it all goes :)
 //= require clipboard
 
+$(document).ready(function(){  
+  
+    var clipboard = new Clipboard('.clipboard');
+    console.log(clipboard);
+      
+});
+
 $('.navbar-toggle').on('click', function(){
     //  $(this).toggleClass('active');
    });
