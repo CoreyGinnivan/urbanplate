fdb44955a3ac7ef761f9601a9644db869f74d50b
corey@ginnivan.net <corey@ginnivan.net>
2018-03-08 21:27:19 +0800

    Fixed default opg settings

--- config.rb
+++ config.rb
@@ -62,7 +62,7 @@ activate :ogp do |ogp|
     og: data.ogp.og
     # from data/ogp/og.yml
   }
-  ogp.base_url = 'http://mysite.tld/'
+  ogp.base_url = 'https://urbanplate.co'
   ogp.blog = true
 end
 
